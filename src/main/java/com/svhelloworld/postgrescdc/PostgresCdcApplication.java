10082484efde331d490a5294c5025cadd0c6bee5
Jason Andersen <jason@agilevmg.com>
2023-01-19 12:34:02 -0800

    Initial commit from Spring Initializr project.

---
+++ src/main/java/com/svhelloworld/postgrescdc/PostgresCdcApplication.java
@@ -0,0 +1,13 @@
+package com.svhelloworld.postgrescdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svhelloworld/postgrescdc/PostgresCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svhelloworld.postgrescdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
